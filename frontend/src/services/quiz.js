4b677fb74715ab2c2d364c842e888beb5923ba7c
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-08-19 00:22:31 +0530

    Update quiz.js

--- frontend/src/services/quiz.js
+++ frontend/src/services/quiz.js
@@ -1,3 +1,18 @@
 import api from "./api";
-export const getQuiz = async (lessonId) => (await api.get(`/quiz/generate?lessonId=${lessonId}`)).data;
-export const submitQuiz = async (quizId, answers) => (await api.post("/quiz/submit", { quizId, answers })).data;
+
+export const getQuiz = async (lessonId) => {
+  const response = await api.get(
+    `/api/quiz/generate?lessonId=${encodeURIComponent(lessonId)}`
+  );
+
+  return response.data;
+};
+
+export const submitQuiz = async (quizId, answers) => {
+  const response = await api.post("/api/quiz/submit", {
+    quizId,
+    answers,
+  });
+
+  return response.data;
+};
