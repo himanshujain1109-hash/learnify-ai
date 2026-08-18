86dd95afdf7bb17d8f82ffe19f97bfad27cfb45c
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-08-19 00:21:35 +0530

    Update lessons.js

--- frontend/src/services/lessons.js
+++ frontend/src/services/lessons.js
@@ -1,3 +1,15 @@
 import api from "./api";
-export const generateLesson=async topicId=>(await api.post("/lessons/generate",{topicId})).data;
-export const getLesson=async id=>(await api.get(`/lessons/${id}`)).data;
+
+export const generateLesson = async (topicId) => {
+  const response = await api.post("/api/lessons/generate", {
+    topicId,
+  });
+
+  return response.data;
+};
+
+export const getLesson = async (id) => {
+  const response = await api.get(`/api/lessons/${id}`);
+
+  return response.data;
+};
