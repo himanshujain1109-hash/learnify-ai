a7974a50ea39209af0de2f00a6b5270ee8f7fc52
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-08-19 00:22:57 +0530

    Update tutor.js

--- frontend/src/services/tutor.js
+++ frontend/src/services/tutor.js
@@ -1,3 +1,10 @@
 import api from "./api";
-export const askTutor = async (documentId, question) =>
-  (await api.post("/tutor/ask", { documentId, question })).data;
+
+export const askTutor = async (documentId, question) => {
+  const response = await api.post("/api/tutor/ask", {
+    documentId,
+    question,
+  });
+
+  return response.data;
+};
