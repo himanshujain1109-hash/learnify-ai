4d988737340cb3b23095d52d1e482ecc7f369c0a
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-08-19 00:41:42 +0530

    Delete backend directory

--- backend/lib/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export default async function connectDB() {
-  if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI is not configured");
-  if (mongoose.connection.readyState === 1) return;
-  await mongoose.connect(process.env.MONGODB_URI);
-}
