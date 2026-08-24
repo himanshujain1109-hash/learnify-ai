fd4c766df9d3abb695fd04533fc0d8b7a44aba92
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-08-24 18:54:13 +0530

    Modify askTutor to accept context parameter
    Refactor askTutor function to include context parameter.

--- frontend/src/services/tutor.js
+++ frontend/src/services/tutor.js
@@ -1,10 +1,13 @@
 import api from "./api";
 
-export const askTutor = async (documentId, question) => {
-  const response = await api.post("/api/tutor/ask", {
-    documentId,
-    question,
-  });
+export const askTutor = async (question, context = "") => {
+  const response = await api.post(
+    "/api/tutor/ask",
+    {
+      question,
+      context,
+    }
+  );
 
   return response.data;
 };
