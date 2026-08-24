c959d6214ee942e72248864db156dd2bb47ab599
himanshujain1109-hash <himanshujain1109@gmail.com>
2026-08-24 21:57:22 +0530

    Delete backend directory

--- backend/models/Document.js
+++
@@ -1,3 +0,0 @@
-import mongoose from "mongoose";
-const schema=new mongoose.Schema({userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},title:String,originalFileName:String,extractedText:String,status:{type:String,default:"uploaded"}},{timestamps:true});
-export default mongoose.models.Document || mongoose.model("Document",schema);
--- backend/models/Quiz.js
+++
@@ -1,3 +0,0 @@
-import mongoose from "mongoose";
-const schema=new mongoose.Schema({lessonId:{type:mongoose.Schema.Types.ObjectId,ref:"Lesson",required:true},questions:Array},{timestamps:true});
-export default mongoose.models.Quiz || mongoose.model("Quiz",schema);
